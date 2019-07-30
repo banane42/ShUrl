a5bc2d6e36183e5699ce721a60d66c524697d38a
Morssel <kleinjak6@gmail.com>
2019-07-30 15:02:26 -0500

    Made naming changes and started UI changes

--- indexCode.js
+++ indexCode.js
@@ -1,26 +1,3 @@
-/* 
- * QR Code generator demo (JavaScript)
- * 
- * Copyright (c) Project Nayuki. (MIT License)
- * https://www.nayuki.io/page/qr-code-generator-library
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- * - The above copyright notice and this permission notice shall be included in
- *   all copies or substantial portions of the Software.
- * - The Software is provided "as is", without warranty of any kind, express or
- *   implied, including but not limited to the warranties of merchantability,
- *   fitness for a particular purpose and noninfringement. In no event shall the
- *   authors or copyright holders be liable for any claim, damages or other
- *   liability, whether in an action of contract, tort or otherwise, arising from,
- *   out of or in connection with the Software or the use or other dealings in the
- *   Software.
- */
-
 "use strict";
 
 
@@ -73,8 +50,12 @@ var app = new function() {
 		}
 		
 		// Get form inputs and compute QR Code
-		var ecl = getInputErrorCorrectionLevel();
-		var text = document.getElementById("text-input").value;
+		
+        var ecl = getInputErrorCorrectionLevel();
+
+//for testing		var text = document.getElementById("text-input").value;
+        var text = "My Test String";
+        
 		var segs = qrcodegen.QrSegment.makeSegments(text);
 		var minVer = parseInt(document.getElementById("version-min-input").value, 10);
 		var maxVer = parseInt(document.getElementById("version-max-input").value, 10);
